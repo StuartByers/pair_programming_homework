89ace4ecc15191ca2795848daa3a1e1ef2e665ae
StuartByers <stuart.byers@outlook.com>
2023-03-01 17:08:15 +0000

    passed subtract test

--- functions_lab_1_start_point/src/python_functions_practice.py
+++ functions_lab_1_start_point/src/python_functions_practice.py
@@ -5,4 +5,6 @@ def add(num1, num2):
     add_result = num1 + num2
     return add_result
 
-# def subtract(num1, num2):
+def subtract(num1, num2):
+    subtract_result = num1 - num2
+    return subtract_result
